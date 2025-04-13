f0f1151101a10174dd9ae99ea3d398bfd5860523
saifiimuhammad <muhammadsaifarain786@gmail.com>
2025-04-13 17:20:25 +0500

    fixed typeerror in api/payments

--- src/app/api/payments/[username]/route.ts
+++ src/app/api/payments/[username]/route.ts
@@ -4,12 +4,14 @@ import { connectDb } from "../../../utils/db";
 
 export async function GET(
   req: Request,
-  { params }: { params: { username: string } }
+  context: { params: { username: string } }
 ) {
   connectDb(process.env.MONGODB_URI as string, "GetmeaLassi");
 
-  const user = await User.findOne({ username: params.username });
-  if (!user) return new Response("User not found", { status: 404 });
+  const user = await User.findOne({ username: context.params.username });
+  if (!user) {
+    return new Response("User not found", { status: 404 });
+  }
 
   const payments = await Payment.find({ reciever: user._id, status: true })
     .sort({ createdAt: -1 })
